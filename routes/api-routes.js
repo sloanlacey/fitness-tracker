93927c2fa3eff34906b63f3d7f385ea50ac17ccc
sloanlacey <sloanlacey89@gmail.com>
2021-03-17 12:16:18 -0700

    Add both GET routes in api-routes.js, start POST route.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,8 +1,29 @@
 const router = require('express').Router();
 const Workout = require('../models/Workout')
 
-router.get('/api/workouts', (req, res) => {
-    res.send('/api/workouts has been reached!');
-  });
+// GET all workouts (view all workouts)
+router.get('/api/workouts', async (req, res) => {
+    try {
+        const viewWorkouts = await Workout.find();
+        res.json(viewWorkouts);
+    } catch (err) {
+        res.status(400);
+        res.send(`Error Message: ${err}`);
+    }
+});
+
+router.get('/api/workouts/range', async (req, res) => {
+    try {
+        const range = await Workout.find().limit(7);
+        res.json(range);
+    } catch (err) {
+        res.status(400);
+        res.send(`Error Message: ${err}`);
+    }
+});
+
+router.post('/api/workouts', (req, res) => {
+    
+});
 
 module.exports = router;
