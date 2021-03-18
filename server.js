3168da9dc392a713e02cb235c650ffd6c901a891
sloanlacey <sloanlacey89@gmail.com>
2021-03-18 16:37:12 -0700

    Deploying with Heroku and Atlas.

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.use(morgan('dev'));
 
