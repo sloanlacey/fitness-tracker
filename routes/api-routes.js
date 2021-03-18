ca77e7b4285ec7635132d46ce9793196078d7047
sloanlacey <sloanlacey89@gmail.com>
2021-03-18 12:49:32 -0700

    Update structure of PUT route in api-routes.js, now new exercises are posting when added.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,7 +2,6 @@ const { Workout } = require('../models');
 const db = require('../models');
 const router = require('express').Router();
 // GET
-
 router.get('/workouts', async (req, res) => {
     try {
         const viewWorkouts = await Workout.find();
@@ -11,7 +10,7 @@ router.get('/workouts', async (req, res) => {
         res.status(500).json({ message: err.message })
     }
 });
-
+// GET
 router.get('/workouts/range', async (req, res) => {
     try {
         const workoutRange = await Workout.find({}). limit(7);
@@ -23,7 +22,7 @@ router.get('/workouts/range', async (req, res) => {
 // POST
 router.post('/workouts', async (req, res) => {
     try {
-        const newWorkout = await Workout.create({});
+        const newWorkout = await Workout.create(req.body);
         res.json(newWorkout);
         res.status(201).json(newWorkout)
     } catch (err) {
@@ -32,17 +31,21 @@ router.post('/workouts', async (req, res) => {
   });
 // PUT
 router.put('/workouts/:id', async (req, res) => {
-    try {
-        const updateWorkout = await res.workout.findByIdAndUpdate(
-            req.params.id,
-            {$push:{
-                exercises: req.body
-            }}
-        );
-        res.json(updateWorkout);
-    } catch (err) {
-        res.status(400).json({ message: err.message })
-    }
+    await Workout.findOneAndUpdate({ _id: req.params.id }, { $push: { exercises: req.body } }, { new: true })
+        .then(workout => {
+            res.json(workout);
+        })
+    // try {
+    //     const updateWorkout = await res.workout.findByIdAndUpdate(
+    //         req.params.id,
+    //         {$push:{
+    //             exercises: req.body
+    //         }}
+    //     );
+    //     res.json(updateWorkout);
+    // } catch (err) {
+    //     res.status(400).json({ message: err.message })
+    // }
 });
 
 module.exports = router;
